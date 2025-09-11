a460df1b523e675566ab7fe1d97519dd082b469c
Juliana-de-Andrade <166360091+Juliana-de-Andrade@users.noreply.github.com>
2025-09-10 21:50:25 -0300

    Update WeatherForecast.cs

--- Challenger.API/WeatherForecast.cs
+++ Challenger.API/WeatherForecast.cs
@@ -1,12 +1,9 @@
-namespace WebApplication2;
+namespace Challenger.API;
 
 public class WeatherForecast
 {
     public DateOnly Date { get; set; }
-
     public int TemperatureC { get; set; }
-
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
     public string? Summary { get; set; }
 }
